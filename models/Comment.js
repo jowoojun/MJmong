2f9e643b92693cb2ac749ecc4b74ac0592d5b6ca
JuYeong <pyeonjy97@gmail.com>
2019-05-27 01:12:09 +0900

    add Comment Model

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  event: { type: Schema.Types.ObjectId, ref: 'Event' },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  content: { type: String },
+  createdAt: { type: Date, default: Date.now },
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
+const Comment = mongoose.model('Comment', schema);
+
+module.exports = Comment;
